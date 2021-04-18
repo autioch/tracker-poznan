d1e232bbabcfba4dc8d7d9ee8dfd17d619389c6b
Jakub Szczepaniak <jakub.szczepaniak@mtab.com>
2021-04-18 16:00:34 +0200

    Data updated.

--- gatherer/parse.mjs
+++ gatherer/parse.mjs
@@ -3,7 +3,7 @@ import './biedronka/parse.mjs';
 import './chatapolska/parse.mjs';
 import './inpost/parse.mjs';
 import './lidl/parse.mjs';
-import './mpk/parse.mjs';
+// import './mpk/parse.mjs';
 import './netto/parse.mjs';
 import './pharmacy/parse.mjs';
 import './zabka/parse.mjs';
