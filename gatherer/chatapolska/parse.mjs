7e44d060afcdb986bcafa1b1d369789631991948
Jakub Szczepaniak <jakub.szczepaniak@mtab.com>
2021-07-10 19:46:30 +0200

    Finished parser for chatapolska.

--- gatherer/chatapolska/parse.mjs
+++ gatherer/chatapolska/parse.mjs
@@ -1,7 +1,22 @@
-import { saveOutputItems } from '../utils.mjs';
-import tmp from './tmp.mjs';
+import fs from 'fs/promises';
 
-const shopList = tmp
+import { joinFromCurrentDir, saveOutputItems } from '../utils.mjs';
+
+const html = await fs.readFile(joinFromCurrentDir(import.meta, 'db', 'page.html')(), 'utf8');
+
+const lines = html.split('\n').map((line) => line.trim());
+
+const indexBeforeList = lines.indexOf('var locations = [');
+const indexAfterList = lines.indexOf(`var map = new google.maps.Map(document.getElementById('map'), {`);
+const linesWithDefinitions = lines.slice(indexBeforeList, indexAfterList);
+
+if (linesWithDefinitions.some((line) => line.includes('<script') || line.includes('()'))) {
+  throw Error('Unsafe data detected in string to be evaluated as code.');
+}
+
+const locations = new Function(`${linesWithDefinitions.join('\n')}return locations;`)(); // eslint-disable-line no-new-func
+
+const shopList = locations
   .filter(([, latitude, longitude]) => !!latitude && !!longitude)
   .map((item, index) => {
     const [text, latitude, longitude] = item;
