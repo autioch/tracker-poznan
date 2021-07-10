a927204628ec1431bc747bc97ef07d636ca3734d
Jakub Szczepaniak <jakub.szczepaniak@mtab.com>
2021-07-10 11:54:20 +0200

    PWA setup.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,6 +2,7 @@ import './favicon.ico';
 import './styles';
 import './512.png';
 import './256.png';
+import './screenshot1.png';
 
 import { loadGroups } from './services/groups';
 import PersistenceService from './services/persistence';
