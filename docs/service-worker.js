fb14f79ad9e0bd14f8382c9af83dbe8a5d9aa2da
Jakub Szczepaniak <jakub.szczepaniak@mtab.com>
2021-12-08 21:57:25 +0100

    Database updated.

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 // Installing service worker
-const CACHE_NAME = 'tracker-poznan-v11';
+const CACHE_NAME = 'tracker-poznan-v12';
 
 /* Add relative URL of all the static content you want to store in
  * cache storage (this will help us use our app offline)*/
