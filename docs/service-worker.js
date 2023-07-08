6d7c54e3ab1421fd2b735be0df906a26bb526a8f
Jakub Szczepaniak <autioch@gmail.com>
2023-07-08 16:08:21 +0200

    Data updated.

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 // Installing service worker
-const CACHE_NAME = 'tracker-poznan-v14';
+const CACHE_NAME = 'tracker-poznan-v15';
 
 /* Add relative URL of all the static content you want to store in
  * cache storage (this will help us use our app offline)*/
